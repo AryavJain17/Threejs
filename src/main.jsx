213ec40db9ef01ac598e04dc4037d846e0f54e8a
AryavJain17 <122171980+AryavJain17@users.noreply.github.com>
2023-07-06 22:27:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as THREE from 'three';
 
-ReactDOM.render(
-        <App/>
-, document.getElementById('root'));
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
